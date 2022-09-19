b73404b1dcfa58ffb1e01c8b2f879da325b98053
= <rafael.borges2698@gmail.com>
2022-09-19 10:22:19 -0300

    finished create task, update task and remove task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,32 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    if(newTaskTitle) {
+      setTasks([...tasks, task])
+      setNewTaskTitle('')
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => id === task.id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }
+    : 
+      task
+    )
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filterTasks = tasks.filter(task => task.id !== id)
+    setTasks(filterTasks)
   }
 
   return (
